8c3506dd6d20784dc858b09c25c957942d6d512b
Anwesha Mondal <159179735+AnweshaMondal@users.noreply.github.com>
2024-12-23 22:39:20 +0530

    Time: 1 ms (65.5%), Space: 45.1 MB (14.29%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -0,0 +1,38 @@
+//for loop is the fastest
+class Solution {
+     public int removeDuplicates(int[] nums) {
+        int count = 0;
+        for(int i=1 ; i<nums.length; i++){
+            if(nums[i]==nums[i-1]){
+                continue;//do nothing, no code execution
+            }
+            else{
+                // i.e. when the consecutive 2 elements are unique
+                nums[count] = nums[i-1];
+                count++; 
+            }
+        }
+         //the last elements into the next position
+            nums[count]= nums[nums.length-1];
+            count++;
+    return count;
+    }
+}
+
+// class Solution {
+//      public int removeDuplicates(int[] nums) {
+//         int count = 1;
+//         int replace = 1; //it can only start from 2nd pos in the array
+//         for(int  i=1; i<nums.length ;i++){
+//             if(nums[i]!=nums[i-1]){
+//                nums[replace]=nums[i];
+//                count++;
+//                replace++;
+//             }
+//             else{
+//                 continue; //go ahead
+//             }
+//         }
+//         return count;      
+//      }
+// }
