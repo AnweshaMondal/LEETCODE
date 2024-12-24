fd28ebe3f05ce0b30747a7f587ccbb3a4741280c
Anwesha Mondal <159179735+AnweshaMondal@users.noreply.github.com>
2024-12-24 22:16:13 +0530

    Time: 1 ms (98.77%), Space: 41.2 MB (45.08%) - LeetHub

--- 1127-last-stone-weight/1127-last-stone-weight.java
+++ 1127-last-stone-weight/1127-last-stone-weight.java
@@ -2,31 +2,61 @@
 // internally as an array for efficiency. It maintains the heap property,
 // not a fully sorted order.
 
+// class Solution {
+//     public int lastStoneWeight(int[] stones) {
+//         //create a max heap 
+//         PriorityQueue<Integer> maxHeap = new  PriorityQueue<>(Collections.reverseOrder());
+
+//         //for each loop add all elements to the heap
+//         for(int element: stones){
+//             maxHeap.add(element);
+//         }
+
+//         //while no of ele in heap>1 remove 2 each time
+//         while(maxHeap.size()>1){
+//             int first = maxHeap.poll();
+//             int second = maxHeap.poll();
+
+//             //if not of equal size
+//             if(first>second){
+//                 maxHeap.add(first-second);//automatically adjusts heap property\U0001f642
+//             }
+//         }
+//         if(!maxHeap.isEmpty()){
+//         return maxHeap.poll();
+//         }
+//         else{
+//             return 0;
+//         }
+//     }
+// }
+
 class Solution {
     public int lastStoneWeight(int[] stones) {
-        //create a max heap 
-        PriorityQueue<Integer> maxHeap = new  PriorityQueue<>(Collections.reverseOrder());
 
-        //for each loop add all elements to the heap
-        for(int element: stones){
-            maxHeap.add(element);
-        }
+     int index = stones.length-1;
+        
+        while(index>0){
 
-        //while no of ele in heap>1 remove 2 each time
-        while(maxHeap.size()>1){
-            int first = maxHeap.poll();
-            int second = maxHeap.poll();
+            Arrays.sort(stones);
+            int f = stones[index];
+            int s = stones[index-1];
 
-            //if not of equal size
-            if(first>second){
-                maxHeap.add(first-second);//automatically adjusts heap property\U0001f642
+            if(f==s){
+                index-=2;
+            }
+            else{
+                index--;
+                stones[index] = f-s;
             }
         }
-        if(!maxHeap.isEmpty()){
-        return maxHeap.poll();
+
+        if(index==0){
+            return stones[0];
         }
         else{
             return 0;
         }
+
     }
 }
