ad3ae99f0f4dd83b9b51255c39c7056c1e07eab3
Anwesha Mondal <159179735+AnweshaMondal@users.noreply.github.com>
2024-12-29 08:51:01 +0530

    Time: 10 ms (89.22%), Space: 58.1 MB (49.65%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,35 +1,35 @@
-class Solution {
-    public boolean containsDuplicate(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+//         HashSet<Integer> set = new HashSet<>();
 
-        for(int i =0; i<nums.length; i++){
+//         for(int i =0; i<nums.length; i++){
             
-            if(!set.contains(nums[i])){   //for HashMap-containsKey
-                set.add(nums[i]);
-            }else{
-                return true;
-            }
-        }
-        return false;
-    }
-}
+//             if(!set.contains(nums[i])){   //for HashMap-containsKey
+//                 set.add(nums[i]);
+//             }else{
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// }
 
 //HASH- SET  is a special data strucyure that never contains any duplicate
 //declare an interger Hast-Set
 
-// class Solution {
-//     public boolean containsDuplicate(int[] nums) {
-// Set<Integer> hashmap = new HashSet<>();
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+Set<Integer> hashmap = new HashSet<>();
 
-// //traverse the array
-// for(int i= 0; i< nums.length ; i++){
-//  if(hashmap.contains(nums[i])){
-//     return true;
-//  }
-//  else{
-//     hashmap.add(nums[i]);
-//  }
-// }
-//  return false;
-//     }
-// }
+//traverse the array
+for(int i= 0; i< nums.length ; i++){
+ if(hashmap.contains(nums[i])){
+    return true;
+ }
+ else{
+    hashmap.add(nums[i]);
+ }
+}
+ return false;
+    }
+}
