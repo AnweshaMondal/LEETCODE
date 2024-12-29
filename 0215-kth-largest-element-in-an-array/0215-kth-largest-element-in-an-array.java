e23f5614b8257e2cb3e30f1d16a3b735ad2dcfe5
Anwesha Mondal <159179735+AnweshaMondal@users.noreply.github.com>
2024-12-29 10:24:08 +0530

    Time: 62 ms (40.04%), Space: 62.1 MB (5.36%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,19 +1,37 @@
 //count distinct elements
 //sort
 
+// class Solution {
+//     public int findKthLargest(int[] nums, int k) {
+//         int n = nums.length;
+//         Arrays.sort(nums);
+
+//         //  int count = 1;
+//         //  for(int i= 1; i<nums.length ; i++){
+//         //     if(nums[i] != nums[i-1]){
+//         //        nums[count] = nums[i];
+//         //        count++;
+//         //     }
+//         //  }
+//         //  return nums[count-k];
+//         return nums[n-k];//hatt faaltu qn
+//     }
+// }
+
+//Priority queue
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        int n = nums.length;
-        Arrays.sort(nums);
 
-        //  int count = 1;
-        //  for(int i= 1; i<nums.length ; i++){
-        //     if(nums[i] != nums[i-1]){
-        //        nums[count] = nums[i];
-        //        count++;
-        //     }
-        //  }
-        //  return nums[count-k];
-        return nums[n-k];
+        PriorityQueue<Integer> q = new  PriorityQueue<>(); //creats a min-heap
+
+        for(int element:nums){
+            q.add(element);
+
+            //filter
+            if(q.size() > k){
+                q.poll(); //pop the smallest one
+            }
+        }
+              return q.peek();
     }
 }
