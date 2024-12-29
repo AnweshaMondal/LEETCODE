6f7c99d8390e8c420f274cbfa9e64b0d6031c295
Anwesha Mondal <159179735+AnweshaMondal@users.noreply.github.com>
2024-12-29 21:32:17 +0530

    Time: 0 ms (100%), Space: 41.6 MB (51.64%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,17 +1,23 @@
-class Solution {
-    public int strStr(String haystack, String needle) {
+// class Solution {
+//     public int strStr(String haystack, String needle) {
 
-        int m = haystack.length(); 
-        int n = needle.length();
+//         int m = haystack.length(); 
+//         int n = needle.length();
       
-    for(int i = 0; i<=m-n ; i++){
-        //Start from index 0 and slide the window by one position until the 
-        //remaining substring in haystack is shorter than needle.
-        if(haystack.substring(i, i+n).equals(needle)){ //ryt index exclusive
-            return i;
-        }   
-    }
+//     for(int i = 0; i<=m-n ; i++){
+//         //Start from index 0 and slide the window by one position until the 
+//         //remaining substring in haystack is shorter than needle.
+//         if(haystack.substring(i, i+n).equals(needle)){ //ryt index exclusive
+//             return i;
+//         }   
+//     }
         
-        return -1;
+//         return -1;
+//     }
+// }
+
+public class Solution {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle); // Returns the first occurrence or -1
     }
 }
