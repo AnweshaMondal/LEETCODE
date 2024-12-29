e6b251e76c99de45d573ee20454c1b8b216e10a4
Anwesha Mondal <159179735+AnweshaMondal@users.noreply.github.com>
2024-12-29 23:11:51 +0530

    Time: 16 ms (19.4%), Space: 49.4 MB (85.47%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,7 +1,35 @@
+//:)
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//         Arrays.sort(nums);
+//         int n = nums.length;
+//         return nums[n/2];
+//     }
+// }
+
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        int n = nums.length;
-        return nums[n/2];
+        int count = 0;
+        int sol = nums[0];
+        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+
+       for(int i = 0 ; i<nums.length; i++){
+
+        if(!map.containsKey(nums[i])){
+           map.put(nums[i],1);
+          }
+         else{
+            count = 0;
+            count = map.get(nums[i])+1;
+           if(count > nums.length/2){
+            sol = nums[i];
+            break;
+           }
+           map.put(nums[i], count);//1->key, 2->value 
+         }
+         
+       }
+
+   return sol;
     }
 }
