8c11a7d25f74c86532be325e0b90c6ef096bdc09
Anwesha Mondal <159179735+AnweshaMondal@users.noreply.github.com>
2024-12-30 16:38:44 +0530

    Time: 1 ms (100%), Space: 47.1 MB (61.77%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
@@ -1,10 +1,34 @@
+// //Brute Force
+// class Solution {
+//     public int[] sortedSquares(int[] nums) {
+//         for(int i = 0; i< nums.length ;i++){
+//             int sq = nums[i]*nums[i];
+//             nums[i] = sq;
+//         }
+//         Arrays.sort(nums);//returns void
+//         return nums;
+//     }
+// }
+
+////Two Pointer
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        for(int i = 0; i< nums.length ;i++){
-            int sq = nums[i]*nums[i];
-            nums[i] = sq;
+        int l = 0; 
+        int r = nums.length-1;
+        int result[] = new int[nums.length]; //new solution array
+
+        for(int i = nums.length-1 ; i>=0 ;i--){
+
+            if(Math.abs(nums[r]) >= Math.abs(nums[l])){
+                result[i] = nums[r]*nums[r];
+                r--;
+            }
+            else{
+               result[i] = nums[l]*nums[l];
+                l++;
+            }
         }
-        Arrays.sort(nums);//returns void
-        return nums;
+        return result;
     }
 }
+
