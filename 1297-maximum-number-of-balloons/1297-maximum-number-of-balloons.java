283bb01b22a2b241a43852a5921b9c2a3a940680
Anwesha Mondal <159179735+AnweshaMondal@users.noreply.github.com>
2024-12-31 20:15:56 +0530

    Time: 1 ms (100%), Space: 42.4 MB (39.76%) - LeetHub

--- 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.java
+++ 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.java
@@ -1,26 +1,50 @@
+// class Solution {
+//     public int maxNumberOfBalloons(String text) {
+
+//         // text = String.toLowerCase(text);//text consists of lower case English letters only
+
+//         HashMap<Character,Integer> map = new HashMap<>();
+//         //Store only characters of Balloon
+//        // Count the occurrences of each character in the text
+//         for (char c : text.toCharArray()) {           //map.get(i) won't work because i isn't a valid key.
+//                                                        //The keys are characters, not indices.
+//             if (c == 'b' || c == 'a' || c == 'l' || c == 'o' || c == 'n') {
+//                 map.put(c, map.getOrDefault(c, 0) + 1);
+//             }
+//         }
+
+//         int min = Integer.MAX_VALUE;
+
+//         min = Math.min(min, map.getOrDefault('b', 0));       // 'b'
+//         min = Math.min(min, map.getOrDefault('a', 0));       // 'a'
+//         min = Math.min(min, map.getOrDefault('l', 0) / 2);   // 'l' appears twice
+//         min = Math.min(min, map.getOrDefault('o', 0) / 2);   // 'o' appears twice
+//         min = Math.min(min, map.getOrDefault('n', 0));       // 'n'
+
+//         return min;
+//     }
+// }
+
 class Solution {
     public int maxNumberOfBalloons(String text) {
-
-        // text = String.toLowerCase(text);//text consists of lower case English letters only
-
-        HashMap<Character,Integer> map = new HashMap<>();
-        //Store only characters of Balloon
-       // Count the occurrences of each character in the text
-        for (char c : text.toCharArray()) {           //map.get(i) won't work because i isn't a valid key.
-                                                       //The keys are characters, not indices.
-            if (c == 'b' || c == 'a' || c == 'l' || c == 'o' || c == 'n') {
-                map.put(c, map.getOrDefault(c, 0) + 1);
+        int b = 0, a = 0, l = 0, o = 0, n = 0;
+        
+        for(char ch : text.toCharArray()){
+            switch(ch){
+                case 'b': ++b;
+                        break;
+                case 'a': ++a;
+                        break;
+                case 'l': ++l;
+                        break;
+                case 'o': ++o;
+                        break;
+                case 'n': ++n;
+                        break;
+            
             }
         }
-
-        int min = Integer.MAX_VALUE;
-
-        min = Math.min(min, map.getOrDefault('b', 0));       // 'b'
-        min = Math.min(min, map.getOrDefault('a', 0));       // 'a'
-        min = Math.min(min, map.getOrDefault('l', 0) / 2);   // 'l' appears twice
-        min = Math.min(min, map.getOrDefault('o', 0) / 2);   // 'o' appears twice
-        min = Math.min(min, map.getOrDefault('n', 0));       // 'n'
-
-        return min;
+        
+        return Math.min(Math.min(o/2, l/2), Math.min(Math.min(b, a), n));
     }
 }
