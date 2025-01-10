548cf04c0d0540f1da94d5fe03d639681e6ad081
Anwesha Mondal <159179735+AnweshaMondal@users.noreply.github.com>
2025-01-10 19:26:21 +0530

    Time: 1 ms (70.25%), Space: 42.3 MB (37.57%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -19,6 +19,7 @@ class Solution {
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> sol = new ArrayList<>();
         Queue<TreeNode> q = new LinkedList<>();
+        TreeNode curr = null;
 
         if (root == null) {
             return sol;
@@ -29,7 +30,7 @@ class Solution {
         while (!q.isEmpty()) {
             int size = q.size(); // Number of nodes at the current level.
             for (int i = 0; i < size; i++) {
-                TreeNode curr = q.poll();
+                  curr = q.poll();
                 
                 // Add children to the queue for the next level.
                 if (curr.left != null) {
@@ -39,11 +40,13 @@ class Solution {
                     q.add(curr.right);
                 }
 
-                // Capture the last node (rightmost) of this level.
-                if (i == size - 1) {
-                    sol.add(curr.val);
-                }
+                
+                // if (i == size - 1) {
+                    // sol.add(curr.val);
+                // }
             }
+            //  Capture the last node (rightmost) of this level.
+             sol.add(curr.val);
         }
 
         return sol;
