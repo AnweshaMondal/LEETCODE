88dee5dfa6b10678a4647cb405b53161b8e74d96
Anwesha Mondal <159179735+AnweshaMondal@users.noreply.github.com>
2025-01-14 00:46:02 +0530

    Time: 2 ms (64.29%), Space: 44.4 MB (49.37%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -13,45 +13,53 @@
  *     }
  * }
  */
-import java.util.*;
-
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        // Map to store the index of each value in inorder array
-        Map<Integer, Integer> inorderIndexMap = new HashMap<>();
-        for (int i = 0; i < inorder.length; i++) {
-            inorderIndexMap.put(inorder[i], i);
+        
+        //store the inorder key in a hashmap to retrieve in O(1) TC
+        HashMap<Integer,Integer> inorderMap = new HashMap<>();
+        
+        int preStart = 0;
+        int preEnd = preorder.length-1;
+        int inStart = 0;
+        int inEnd = inorder.length-1;
+
+        //Store the items of Hashmap
+        for(int i = 0; i<inorder.length; i++)
+        {
+               inorderMap.put(inorder[i],i); //key,value
         }
-        // Start building the tree
-        return buildTreeHelper(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1, inorderIndexMap);
+
+        return buildTree(preorder, preStart, preEnd ,inorder , inStart, inEnd, inorderMap);
+        
     }
 
-    private TreeNode buildTreeHelper(int[] preorder, int preStart, int preEnd, 
-                                     int[] inorder, int inStart, int inEnd, 
-                                     Map<Integer, Integer> inorderIndexMap) {
-        // Base case: if no elements are left to process
-        if (preStart > preEnd || inStart > inEnd) {
-            return null;
-        }
 
-        // The first element in preorder is the root
-        int rootValue = preorder[preStart];
-        TreeNode root = new TreeNode(rootValue);
+    TreeNode buildTree(int[] preOrder, int preStart, int preEnd,
+                       int[] inOrder, int inStart, int inEnd,
+                       HashMap<Integer,Integer> inorder){
 
-        // Find the index of the root in the inorder array
-        int rootIndex = inorderIndexMap.get(rootValue);
+                        if(preStart>preEnd || inStart>inEnd){
+                            return null;
+                        }
 
-        // Number of elements in the left subtree
-        int leftSize = rootIndex - inStart;
+           int rootval = preOrder[preStart];
+           TreeNode root = new TreeNode(rootval); //create thr root node
 
-        // Recursively construct the left subtree
-        root.left = buildTreeHelper(preorder, preStart + 1, preStart + leftSize, 
-                                    inorder, inStart, rootIndex - 1, inorderIndexMap);
+           //No of nodes in left sub Tree
+           int no_left_node = inorder.get(rootval)-inStart;
 
-        // Recursively construct the right subtree
-        root.right = buildTreeHelper(preorder, preStart + leftSize + 1, preEnd, 
-                                     inorder, rootIndex + 1, inEnd, inorderIndexMap);
+           //rootIndex
+           int rootIndex = inorder.get(rootval);
 
+           root.left = buildTree(preOrder, preStart + 1, preStart + no_left_node, 
+                         inOrder, inStart, rootIndex - 1, inorder);
+
+           root.right = buildTree(preOrder, preStart + no_left_node+1, preEnd ,
+                         inOrder, rootIndex+1, inEnd , inorder);
+                         
+                         
         return root;
+           
     }
 }
