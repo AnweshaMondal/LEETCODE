27b848a3b2371bf375eeaa39bcfdfc4980c42251
Anwesha Mondal <159179735+AnweshaMondal@users.noreply.github.com>
2025-02-07 00:02:26 +0530

    Time: 3 ms (55.69%), Space: 61.4 MB (5.49%) - LeetHub

--- 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.java
+++ 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.java
@@ -1,48 +1,48 @@
-// class Solution {
-//     public int waysToSplitArray(int[] nums) {
-
-//       int[] prefix = new int[nums.length];  
-//       prefix[0] = nums[0];
-//       int n = nums.length;
-
-//       for(int i = 1 ; i<nums.length; i++)
-//       {
-//         prefix[i] = prefix[i-1]+nums[i];
-//       }
-      
-//        int totalsum = prefix[n-1];
-//        int count = 0;
-//        for(int i = 0; i< n-1; i++)
-//        {
-//           int currsum = totalsum - prefix[i];
-//           if(prefix[i] >= currsum)
-//           {
-//             count++;
-//           }
-//        }
-
-//        return count;  
-//     }
-// }
-
 class Solution {
     public int waysToSplitArray(int[] nums) {
-        long leftSum = 0;  // Use long to prevent overflow
-        long totalSum = 0;
 
-        for (int num : nums) {
-            totalSum += num;
-        }
+      long[] prefix = new long[nums.length];  
+      prefix[0] = nums[0];
+      int n = nums.length;
 
-        int count = 0;
-        for (int i = 0; i < nums.length - 1; i++) {
-            leftSum += nums[i];
-            long rightSum = totalSum - leftSum;
-            if (leftSum >= rightSum) {
-                count++;
-            }
-        }
+      for(int i = 1 ; i<nums.length; i++)
+      {
+        prefix[i] = prefix[i-1]+nums[i];
+      }
+      
+       long totalsum = prefix[n-1];
+       int count = 0;
+       for(int i = 0; i< n-1; i++)
+       {
+          long currsum = totalsum - prefix[i];
+          if(prefix[i] >= currsum)
+          {
+            count++;
+          }
+       }
 
-        return count;
+       return count;  
     }
 }
+
+// class Solution {
+//     public int waysToSplitArray(int[] nums) {
+//         long leftSum = 0;  // Use long to prevent overflow
+//         long totalSum = 0;
+
+//         for (int num : nums) {
+//             totalSum += num;
+//         }
+
+//         int count = 0;
+//         for (int i = 0; i < nums.length - 1; i++) {
+//             leftSum += nums[i];
+//             long rightSum = totalSum - leftSum;
+//             if (leftSum >= rightSum) {
+//                 count++;
+//             }
+//         }
+
+//         return count;
+//     }
+// }
