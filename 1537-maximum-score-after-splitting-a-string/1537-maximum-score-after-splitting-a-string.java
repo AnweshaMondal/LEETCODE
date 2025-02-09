dedebd5aa55c657053518b15bdd8ef6f40d8d2cc
Anwesha Mondal <159179735+AnweshaMondal@users.noreply.github.com>
2025-02-09 22:13:12 +0530

    Time: 3 ms (23.84%), Space: 41.4 MB (86.2%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.java
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.java
@@ -1,33 +1,50 @@
-class Solution {
-    public int maxScore(String s) {
+// class Solution {
+//     public int maxScore(String s) {
         
-        int leftzeroes = 0;
-        int totalones = 0;
-        int maxscore = 0;
+//         int leftzeroes = 0;
+//         int totalones = 0;
+//         int maxscore = 0;
 
-        for(int i = 0; i<s.length(); i++)
-        {
-            if(s.charAt(i)=='1')
-            {
-                totalones+=1;
-            }
+//         for(int i = 0; i<s.length(); i++)
+//         {
+//             if(s.charAt(i)=='1')
+//             {
+//                 totalones+=1;
+//             }
+//         }
+
+//         for(int i = 0; i<s.length()-1; i++)//right most element must be left
+//         {
+//            if(s.charAt(i)=='0')
+//            {
+//             leftzeroes +=1;
+//            }
+//            else{
+//             //s.chatAt(i)=='1'
+//             totalones--;
+//            }
+
+//            int score = leftzeroes + totalones;
+//            maxscore = Math.max(score,maxscore); 
+//         }
+
+//         return maxscore;
+//     }
+// }
+
+public class Solution {
+    public int maxScore(String s) {
+        int maxScore = 0;
+        int countZerosLeft = 0;
+        int countOnesRight = (int) s.chars().filter(ch -> ch == '1').count();
+
+
+        for (int i = 0; i < s.length() - 1; i++) {
+            countZerosLeft += s.charAt(i) == '0' ? 1 : 0;
+            countOnesRight -= s.charAt(i) == '1' ? 1 : 0;
+            maxScore = Math.max(maxScore, countZerosLeft + countOnesRight);
         }
 
-        for(int i = 0; i<s.length()-1; i++)//right most element must be left
-        {
-           if(s.charAt(i)=='0')
-           {
-            leftzeroes +=1;
-           }
-           else{
-            //s.chatAt(i)=='1'
-            totalones--;
-           }
-
-           int score = leftzeroes+totalones;
-           maxscore = Math.max(score,maxscore); 
-        }
-
-        return maxscore;
+        return maxScore;
     }
 }
