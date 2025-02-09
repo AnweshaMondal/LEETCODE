1a71c9c2686b771d8d37b60b8ccdf12566e6c46f
Anwesha Mondal <159179735+AnweshaMondal@users.noreply.github.com>
2025-02-09 11:35:53 +0530

    Time: 1153 ms (5.12%), Space: 46.8 MB (14.11%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
@@ -1,34 +1,44 @@
-import java.util.*;
-
 class Solution {
     public int countPalindromicSubsequence(String s) {
-        int count = 0;
         
-        // Iterate through each character from 'a' to 'z'
-        for (char c = 'a'; c <= 'z'; c++) {
-            int first = -1, last = -1;
-            
-            // Find first and last occurrence of character 'c'
-            for (int i = 0; i < s.length(); i++) {
-                if (s.charAt(i) == c) {
-                    if (first == -1) first = i; // First occurrence
-                    last = i; // Always update last occurrence
-                }
-            }
-            
-            // If we have at least one character in between
-            if (first != -1 && last != -1 && last - first > 1) {
-                HashSet<Character> uniqueMiddles = new HashSet<>();
-                
-                // Count unique middle characters
-                for (int j = first + 1; j < last; j++) {
-                    uniqueMiddles.add(s.charAt(j));
-                }
-                
-                count += uniqueMiddles.size();
+      int n = s.length(); 
+      HashSet<String> set = new HashSet<>();
+      HashSet<Character> letters = new HashSet<>();
+
+      for(char ch : s.toCharArray()) //insert all chars that has occurred in input String
+      {
+          letters.add(ch);
+      }
+       
+      for (Character c : letters)
+      {
+         
+        int f = -1;
+        int l = -1;
+
+        for(int j = 0 ; j<n ;j++)
+        {
+            if(s.charAt(j)== c)
+            {
+                if(f==-1) f = j;
+                l = j;//iterate to the last occurred c in the String
             }
         }
-        
-        return count;
+
+         if(l-f >1)
+            {
+                //count all unique palindrome substrings
+                // Set<Character> between = new HashSet<>();
+                for(int k=f+1 ;k<l ;k++)
+                {
+                    set.add(""+s.charAt(f)+s.charAt(k)+s.charAt(l));//s.charAt(f) + s.charAt(k) + s.charAt(l) performs integer addition because char values are treated as ASCII integers when added.
+                                                                    //add an empty string
+                }
+               
+            }
+
+      }
+
+      return set.size();
     }
 }
