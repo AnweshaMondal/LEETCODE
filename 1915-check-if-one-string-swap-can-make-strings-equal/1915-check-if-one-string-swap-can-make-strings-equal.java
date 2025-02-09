2cbf11c5fb2791a4c6f5dd2a5bb48c5730c8f2f5
Anwesha Mondal <159179735+AnweshaMondal@users.noreply.github.com>
2025-02-09 20:47:51 +0530

    Time: 0 ms (100%), Space: 41.5 MB (85.8%) - LeetHub

--- 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.java
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.java
@@ -16,15 +16,17 @@ class Solution {
             if(s1.charAt(i) != s2.charAt(i))
             {
               count ++;
-              if(count ==1)
-               {
-                 l = i;
-               }
+              if(l== -1) l = i;
+              else r = i ;
+            //   if(count ==1)
+            //    {
+            //      l = i;
+            //    }
 
-             if(count ==2)
-               {
-                r = i;
-               }
+            //  if(count ==2)
+            //    {
+            //     r = i;
+            //    }
             }
         }
 
@@ -39,3 +41,25 @@ class Solution {
         
     }
 }
+
+//gpt
+// class Solution {
+//     public boolean areAlmostEqual(String s1, String s2) {
+//         if (s1.equals(s2)) return true; // Already equal
+        
+//         int first = -1, second = -1;
+//         int count = 0;
+
+//         for (int i = 0; i < s1.length(); i++) {
+//             if (s1.charAt(i) != s2.charAt(i)) {
+//                 count++;
+//                 if (count > 2) return false; // More than 2 mismatches → can't fix with one swap
+//                 if (first == -1) first = i;
+//                 else second = i;
+//             }
+//         }
+
+//         return (count == 2 && s1.charAt(first) == s2.charAt(second) && s1.charAt(second) == s2.charAt(first));
+//     }
+// }
+
