db3723e4e59560d0cb8a68de27989fc88e690257
Anwesha Mondal <159179735+AnweshaMondal@users.noreply.github.com>
2025-02-09 20:30:59 +0530

    Time: 0 ms (100%), Space: 41.6 MB (16.86%) - LeetHub

--- 1927-maximum-ascending-subarray-sum/1927-maximum-ascending-subarray-sum.java
+++ 1927-maximum-ascending-subarray-sum/1927-maximum-ascending-subarray-sum.java
@@ -25,9 +25,12 @@ class Solution {
                     sum = 0;
             }
 
-                //in case the adjacent 2 elements are equal or less than the previous one
-                // inlen = 1;
-                i = j ;
+            //in case the adjacent 2 elements are equal or less than the previous one
+            // while(j<nums.length && nums[j-1]>=nums[j]) 
+            // {
+            //     j++;
+            // }->TLE
+                i = j;
                 j = i+1;
             
         }
@@ -35,3 +38,21 @@ class Solution {
         return maxsum;
     }
 }
+
+// class Solution {
+//     public int maxAscendingSum(int[] nums) {
+//         int maxsum = nums[0];
+//         int sum = nums[0];
+
+//         for (int i = 1; i < nums.length; i++) {
+//             if (nums[i] > nums[i - 1]) {
+//                 sum += nums[i];  // Keep adding in ascending order
+//             } else {
+//                 sum = nums[i];  // Reset the sum for a new sequence
+//             }
+//             maxsum = Math.max(maxsum, sum);
+//         }
+
+//         return maxsum;
+//     }
+// }
