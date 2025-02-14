c98057d53bf046534840d56eecb533e77996186a
Anwesha Mondal <159179735+AnweshaMondal@users.noreply.github.com>
2025-02-14 12:54:26 +0530

    Time: 11 ms (13.68%), Space: 45.3 MB (6.89%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.java
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.java
@@ -1,26 +1,48 @@
 // //Removing any part or any given pattern from String == Use Stack
 // //Find the first ocurrence of part -> remove -> repeat untill all part is removed
 
-// class Solution {
-//     public String removeOccurrences(String s, String part) {
-        
-//         Stack<Character> st = new Stack<>();
-//         int partlen = part.length();
-//         char init = part.charAt(0);
-      
-//       for(int i= 0; i<s.length(); i++)
-//       {
-//         st.push(s.charAt(i));
-//         //  If stack size is greater than or equal to the part length, check for match
-//         if(st.size()>= partlen)
-//         {
-            
-//         }
-//       }
+import java.util.Stack;
 
-      
-//     }
-// }
+class Solution {
+    public String removeOccurrences(String s, String part) {
+        Stack<Character> st = new Stack<>();
+        int partlen = part.length();
+
+        for (int i = 0; i < s.length(); i++) {  
+            st.push(s.charAt(i));
+
+            // Check if the stack contains the part at the top
+            if (st.size() >= partlen && containsPart(part, partlen, st)) {
+                for (int j = 0; j < partlen; j++) {
+                    st.pop(); // Remove the part from the stack
+                }
+            }
+        }
+
+        // Correct way to build the final string
+        StringBuilder res = new StringBuilder();
+        while (!st.isEmpty()) {
+            res.append(st.pop());
+        }
+        res.reverse(); // Reverse since stack stores elements in LIFO order
+
+        return res.toString();
+    }  
+
+    boolean containsPart(String part, int partlen, Stack<Character> st) {  
+        Stack<Character> temp = new Stack<>();
+        temp.addAll(st); // Copy contents of st into temp  
+
+        for (int i = partlen - 1; i >= 0; i--) {
+            // Ensure stack is not empty before calling peek()
+            if ( temp.peek() != part.charAt(i)) {
+                return false;
+            }
+            temp.pop();              
+        }
+        return true;
+    }
+}
 
 //String Builder
 // class Solution {
@@ -40,15 +62,16 @@
 // }
 
 //By String In built Function
-class Solution {
-    public String removeOccurrences(String s, String part) {
+// class Solution {
+//     public String removeOccurrences(String s, String part) {
 
-        while(s.contains(part))//contains() is also a function of HashSet
-        {
-          int partIndex = s.indexOf(part);
-          s = s.substring(0, partIndex)+s.substring(partIndex+part.length());
-        }
+//         while(s.contains(part))//contains() is also a function of HashSet
+//         {
+//           int partIndex = s.indexOf(part);
+//           s = s.substring(0, partIndex)+s.substring(partIndex+part.length());
+//         }
 
-        return s;
-    }
-}
+//         return s;
+//     }
+// }
+
