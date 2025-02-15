b4d43d8a1589a95856d48dc8788079d458d0a949
Anwesha Mondal <159179735+AnweshaMondal@users.noreply.github.com>
2025-02-15 13:18:00 +0530

    Time: 155 ms (29.1%), Space: 70.6 MB (59.42%) - LeetHub

--- 3332-minimum-operations-to-exceed-threshold-value-ii/3332-minimum-operations-to-exceed-threshold-value-ii.java
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/3332-minimum-operations-to-exceed-threshold-value-ii.java
@@ -1,30 +1,22 @@
-import java.util.PriorityQueue;
-
 class Solution {
     public int minOperations(int[] nums, int k) {
-        PriorityQueue<Long> q = new PriorityQueue<>(); // Use long to avoid integer overflow
-        int operations = 0;
+        
+      PriorityQueue<Long> q = new PriorityQueue<Long>();
+      int operations = 0;
 
-        // Insert all elements into the min heap
-        for (int num : nums) {
-            q.add((long) num);
-        }
-
-        // Continue processing while the smallest element is < k
-        while (q.size() >= 2 && q.peek() < k) {
-            // if {
-                // If there's only one element left and it's < k, we can't perform any more operations
-                // break;
-            // }
-
-            long min = q.poll(); // Get the smallest element
-            long secmin = q.poll(); // Get the second smallest element
-            long mediate = 2 * min + secmin; // Compute new number
-
-            q.add(mediate); // Insert back into heap
-            operations++;
-        }
-
-        return operations;
+      for(int i = 0; i<nums.length; i++)
+      {
+        q.add((long)nums[i]); //Insert All elements inside the queue
+      }
+     
+     while(q.size()>=2 && q.peek()< k) //untill the least element is less than K , loop through
+     {
+       long min = q.poll();
+       long secmin = q.poll();
+       long mediate = 2*min +secmin;
+       q.add(mediate);
+       operations++;
+     }
+      return operations;
     }
 }
