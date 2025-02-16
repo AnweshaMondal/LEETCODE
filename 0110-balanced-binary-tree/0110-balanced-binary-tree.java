6cbcdeb9089c4a2efa45db83248914521e27e7f2
Anwesha Mondal <159179735+AnweshaMondal@users.noreply.github.com>
2025-02-16 12:27:29 +0530

    Time: 0 ms (100%), Space: 44.6 MB (32.83%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -15,7 +15,11 @@
  */
 class Solution {
     public boolean isBalanced(TreeNode root) {
-        return height(root) > -1 ;
+        // int lh = height(root.left);
+        // int rh = height(root.right);
+        // return (Math.abs(lh-rh)>1) ;    // Unbalanced
+
+        return height(root)!= -1;
     }
 
     int height(TreeNode root){
@@ -32,3 +36,7 @@ class Solution {
         return Math.max(lh,rh)+1 ;
     }
 }
+//WRITE DEPTH FUNCTION AS HELPER FUNCTION
+//IN THE GIVEN FUNCTION , CALCULATE THE DEPTH OF LEFT ANF RIGHT SUB TREE 
+//CHECK IF (MATH.ABS(LH-RH)>1 => return false;)
+//else, do nothing, depth of the right and left node is the only thing to calculate
