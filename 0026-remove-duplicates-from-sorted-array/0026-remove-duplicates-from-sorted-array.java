e4bdbbfee6fc9722861e79092d5dd7f6c1fac81e
Anwesha Mondal <159179735+AnweshaMondal@users.noreply.github.com>
2025-03-29 20:39:04 +0530

    Time: 0 ms (100%), Space: 44.9 MB (60.28%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,56 +1,21 @@
-// //for loop is the fastest
-// class Solution {
-//      public int removeDuplicates(int[] nums) {
-//         int count = 0;
-//         for(int i=1 ; i<nums.length; i++){
-//             if(nums[i]==nums[i-1]){
-//                 continue;//do nothing, no code execution
-//             }
-//             else{
-//                 // i.e. when the consecutive 2 elements are unique
-//                 nums[count] = nums[i-1];
-//                 count++; 
-//             }
-//         }
-//          //the last elements into the next position
-//             nums[count]= nums[nums.length-1];
-//             count++;
-//     return count;
-//     }
-// }
-
-// class Solution {
-//      public int removeDuplicates(int[] nums) {
-//         int count = 1;
-//         int replace = 1; //it can only start from 2nd pos in the array
-//         for(int  i=1; i<nums.length ;i++){
-//             if(nums[i]!=nums[i-1]){
-//                nums[replace]=nums[i];
-//                count++;
-//                replace++;
-//             }
-            
-//         }
-//         return count;      
-//      }
-// }
-
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int count = 0;
-
-        for (int i = 1; i < nums.length - count; i++) {
-            if (nums[i - 1] == nums[i]) {
-                count++;
-
-                // Shift elements to the left
-                for (int j = i; j <= nums.length-count; j++) {
-                    nums[j - 1] = nums[j];
-                }
-
-                i--; // Decrement `i` to recheck the current position after shifting
+        
+        int i = 1;
+        int k = 1;
+        
+        while(i<nums.length)
+        {
+            if(nums[i] == nums[i-1])
+            {
+              i++;
+            }
+            else //ie the cosecutive elements are not equal
+            {
+               nums[k++] = nums[i++];
             }
         }
-        return nums.length - count;
+
+        return k;
     }
 }
