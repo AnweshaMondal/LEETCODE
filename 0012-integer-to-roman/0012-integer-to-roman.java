fdbb652d804df274979ec024008495de7200fd47
Anwesha Mondal <159179735+AnweshaMondal@users.noreply.github.com>
2025-04-25 18:50:34 +0530

    Time: 4 ms (42.81%), Space: 44.4 MB (71.73%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String intToRoman(int num) {
+        
+        //Instead of using a map, use 2 arrays
+        int index[] = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String symbol[] = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        String res = "";
+        int len  = index.length;
+
+        for(int i = 0; num!=0  ; i++)
+        {
+           while(num >= index[i] && i< len)
+           {
+             num -= index[i];
+              res+=symbol[i];
+           }
+        //    else
+        //    {
+        //      continue ;
+        //    }
+        } 
+        return res; 
+    }
+}
