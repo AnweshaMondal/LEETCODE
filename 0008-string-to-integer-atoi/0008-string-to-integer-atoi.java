9d20bb2be753fa1749e98bef686d23d87b7a3866
Anwesha Mondal <159179735+AnweshaMondal@users.noreply.github.com>
2025-04-26 11:38:20 +0530

    Time: 1 ms (100%), Space: 42.3 MB (81.38%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -0,0 +1,48 @@
+// Summary:
+// Always increment i after reading a character.
+// Handle whitespace and sign only once.
+// Use (int) to cast long to int.
+// Use Character.isDigit() instead of c >= '0' && c <= '9' for better readability.
+//Don't forget to increment in a while loop
+
+class Solution {
+    public int myAtoi(String s) {
+        int flag = 0;
+        long res = 0L;
+        int i = 0;
+        int n = s.length();
+
+        // Step 1: skip the leading white space
+        while (i < n && s.charAt(i) == ' ') {
+            i++;
+        }
+
+        // Check if string is empty after whitespaces
+        if (i >= n) return 0;
+
+        // Step 2: Evaluate the sign after the whitespace
+        if (s.charAt(i) == '-') {
+            flag = 1;
+            i++;
+        } else if (s.charAt(i) == '+') {
+            i++;
+        }
+
+        // Step 3: Evaluate the characters thereafter
+        while (i < n && Character.isDigit(s.charAt(i))) {
+            res = res * 10 + (s.charAt(i) - '0');
+
+            // Step 4: Clamp the result early if out of bounds
+            if (flag == 0 && res > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+            if (flag == 1 && -res < Integer.MIN_VALUE) return Integer.MIN_VALUE;
+
+            i++;
+        }
+
+        if (flag == 1) {
+            res = -res;
+        }
+
+        return (int) res;
+    }
+}
