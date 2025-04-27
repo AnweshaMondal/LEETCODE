cbeeeb9a457b5d3f288b6ad56996c262c157d60a
Anwesha Mondal <159179735+AnweshaMondal@users.noreply.github.com>
2025-04-27 09:06:33 +0530

    Time: 2 ms (97.39%), Space: 41.7 MB (82.21%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,52 +1,80 @@
+// class Solution {
+//     public boolean isValid(String s) {
+        
+//         Stack<Character> st = new Stack<>();
+//         int n = s.length();
+
+//         for(int i = 0; i<n; i++)
+//         {
+//             char ch = s.charAt(i);
+
+//             //check if an  opening bracket
+//             if(ch =='(' || ch == '{' || ch == '[')
+//             {
+//                 st.push(ch);
+//             }
+
+//             //if it is a closing bracket
+//             else if(!st.isEmpty() && (ch == '}' || ch == ')'||ch == ']'))
+//             {
+//                 if(st.peek() == '(' && ch == ')')
+//                 {
+//                     st.pop();
+//                 }
+
+//                 else if(!st.isEmpty() && st.peek() == '{' && ch == '}')
+//                 {
+//                     st.pop();
+//                 }
+
+//                 else if(!st.isEmpty() && st.peek() == '[' && ch == ']')
+//                 {
+//                     st.pop();
+//                 }
+
+//                 else{
+//                     return false;
+//                 }
+//             }
+
+//             // else //if( st.isEmpty() && (ch == ']'|| ch=='}'||ch ==')'))
+//             // {
+//             //    return false;
+//             // }
+
+//         }
+
+//         if(!st.isEmpty()) return false;
+        
+//         return true;
+        
+//     }
+// }
+
 class Solution {
     public boolean isValid(String s) {
-        
         Stack<Character> st = new Stack<>();
         int n = s.length();
 
-        for(int i = 0; i<n; i++)
-        {
+        for(int i = 0; i < n; i++) {
             char ch = s.charAt(i);
 
-            //check if an  opening bracket
-            if(ch =='(' || ch == '{' || ch == '[')
-            {
+            if(ch == '(' || ch == '{' || ch == '[') {
                 st.push(ch);
-            }
+            } 
+            else {
+                if(st.isEmpty()) return false;
 
-            //if it is a closing bracket
-            else if(!st.isEmpty() && (ch == '}' || ch == ')'||ch == ']'))
-            {
-                if(st.peek() == '(' && ch == ')')
-                {
-                    st.pop();
-                }
+                char top = st.pop();
 
-                else if(!st.isEmpty() && st.peek() == '{' && ch == '}')
-                {
-                    st.pop();
-                }
-
-                else if(!st.isEmpty() && st.peek() == '[' && ch == ']')
-                {
-                    st.pop();
-                }
-
-                else{
+                if((ch == ')' && top != '(') ||
+                   (ch == '}' && top != '{') ||
+                   (ch == ']' && top != '[')) {
                     return false;
                 }
             }
-
-            else //if( st.isEmpty() && (ch == ']'|| ch=='}'||ch ==')'))
-            {
-               return false;
-            }
-
         }
 
-        if(!st.isEmpty()) return false;
-        
-        return true;
-        
+        return st.isEmpty();
     }
 }
