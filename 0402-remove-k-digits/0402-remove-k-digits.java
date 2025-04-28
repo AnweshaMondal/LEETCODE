c774645a2384db1f466fbfe3b4aa859e690f58fa
Anwesha Mondal <159179735+AnweshaMondal@users.noreply.github.com>
2025-04-28 23:03:09 +0530

    Time: 26 ms (38.56%), Space: 45.7 MB (56.02%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.java
+++ 0402-remove-k-digits/0402-remove-k-digits.java
@@ -1,48 +1,87 @@
-class Solution {
-    public String removeKdigits(String s, int k) {
+// class Solution {
+//     public String removeKdigits(String s, int k) {
         
+//         Stack<Character> st = new Stack<>();
+//         st.add(s.charAt(0));
+
+//         // if(k>s.length()) return"0";
+
+//         for(int i = 1; i<s.length(); i++)
+//         {
+//             while(k>0 && !st.isEmpty() && st.peek()>s.charAt(i))
+//             {
+//                 st.pop();
+//                 k--;
+//             }
+
+
+//             st.push(s.charAt(i)); //push the current char
+
+
+//             //remove leading 0
+//             if(st.size() == 1 && st.peek() == '0')
+//             {
+//                st.pop();
+//             }
+//         }
+//         //in case the string ended but k didn't turned 0 :Example-3
+//         while(k>0 && !st.isEmpty())
+//         {
+//             st.pop();
+//             k--;
+//         }
+
+//     //Form the number by emptying the Stack, then reverse it
+//     StringBuilder sb = new StringBuilder();
+//     while(!st.isEmpty())
+//     {
+//         sb.append(st.pop());
+//     }
+
+//     sb.reverse();
+//     String res = sb.toString();
+//     if (res.isEmpty()) return "0";
+//     return res;
+
+
+//     }
+// }
+
+class Solution {
+    public String removeKdigits(String num, int k) {
         Stack<Character> st = new Stack<>();
-        st.add(s.charAt(0));
 
-        // if(k>s.length()) return"0";
-
-        for(int i = 1; i<s.length(); i++)
-        {
-            while(k>0 && !st.isEmpty() && st.peek()>s.charAt(i))
-            {
+        for (int i = 0; i < num.length(); i++) {
+            char c = num.charAt(i);
+            
+            while (!st.isEmpty() && k > 0 && st.peek() > c) {
                 st.pop();
                 k--;
             }
-
-
-            st.push(s.charAt(i)); //push the current char
-
-
-            //remove leading 0
-            if(st.size() == 1 && st.peek() == '0')
-            {
-               st.pop();
-            }
+            st.push(c);
         }
-        //in case the string ended but k didn't turned 0 :Example-3
-        while(k>0 && !st.isEmpty())
-        {
+
+        // If k is still left
+        while (k > 0 && !st.isEmpty()) {
             st.pop();
             k--;
         }
 
-    //Form the number by emptying the Stack, then reverse it
-    StringBuilder sb = new StringBuilder();
-    while(!st.isEmpty())
-    {
-        sb.append(st.pop());
-    }
+        // Build the number
+        StringBuilder sb = new StringBuilder();
+        while (!st.isEmpty()) {
+            sb.append(st.pop());
+        }
+        sb.reverse();
 
-    sb.reverse();
-    String res = sb.toString();
-    if (res.isEmpty()) return "0";
-    return res;
+        // Remove leading zeros
+        int idx = 0;
+        while (idx < sb.length() && sb.charAt(idx) == '0') {
+            idx++;
+        }
 
+        String result = sb.substring(idx);
 
+        return result.isEmpty() ? "0" : result;
     }
 }
