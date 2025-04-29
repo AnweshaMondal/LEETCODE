a8b18ca0488d529ec0cbbf0cc642ff84b168203c
Anwesha Mondal <159179735+AnweshaMondal@users.noreply.github.com>
2025-04-29 17:31:20 +0530

    Time: 0 ms (100%), Space: 41.4 MB (78.08%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -6,7 +6,7 @@
       
 //     for(int i = 0; i<=m-n ; i++){
 //         //Start from index 0 and slide the window by one position until the 
-//         //remaining substring in haystack is shorter than needle.
+//         //remaining substring right side in haystack is shorter than needle.
 //         if(haystack.substring(i, i+n).equals(needle)){ //ryt index exclusive
 //             return i;
 //         }   
@@ -16,8 +16,43 @@
 //     }
 // }
 
-public class Solution {
+// public class Solution {
+//     public int strStr(String haystack, String needle) {
+//         return haystack.indexOf(needle); // Returns the first occurrence or -1
+//     }
+// }
+
+
+class Solution {
     public int strStr(String haystack, String needle) {
-        return haystack.indexOf(needle); // Returns the first occurrence or -1
+     
+      int m = haystack.length();
+      int n = needle.length();
+
+      for(int i= 0; i<=m-n; i++)
+      {
+        if(haystack.charAt(i) == needle.charAt(0))
+        {
+            if(isSameString(i,haystack,needle)) return i;
+        }
+      }
+     return -1;
     }
+
+boolean isSameString(int indx, String haystack, String needle)
+{
+   int i = indx;
+   for(int j = 0; j<needle.length(); j++)
+   {
+     if(haystack.charAt(i)!= needle.charAt(j))
+     {
+        return false;
+     }
+
+      i++;
+      
+   }
+   return true;
 }
+}
+
