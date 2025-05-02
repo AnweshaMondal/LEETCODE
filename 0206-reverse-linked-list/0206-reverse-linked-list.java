49a423ef9c3ed6b35407187ac98355ab26da9683
Anwesha Mondal <159179735+AnweshaMondal@users.noreply.github.com>
2025-05-02 07:05:05 +0530

    Time: 0 ms (100%), Space: 42.8 MB (19%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -8,24 +8,45 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
  //Inplace
+// class Solution {
+//     public ListNode reverseList(ListNode head) {
+
+//         if(head == null) return head; //empty list
+//         if(head.next == null) return head; //size of the linkedlist is 1
+    
+//         ListNode prev = head; 
+//         ListNode curr = head.next;
+//         head.next = null;
+
+//         while(curr!= null)
+//         {
+//             ListNode ahead = curr.next;
+//             curr.next = prev;
+//             prev = curr;
+//             curr = ahead;
+//         }
+//         return prev;
+//     }
+// }
+
+//With Memory->Insert at first
 class Solution {
     public ListNode reverseList(ListNode head) {
-        
-        if(head == null) return head; //empty list
-        if(head.next == null) return head; //size of the linkedlist is 1
-    
-        ListNode prev = head; 
-        ListNode curr = head.next;
-        head.next = null;
 
-        while(curr!= null)
+        if(head == null) return null;
+        ListNode temp = head ;
+        ListNode copy = null;//initialized to null 
+
+        while(temp != null)
         {
-            ListNode ahead = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = ahead;
+            ListNode newNode = new ListNode(temp.val);
+            newNode.next = copy;
+            copy = newNode;//traverses along the copy list
+            temp = temp.next;//traverses the main/given list
         }
-        return prev;
+
+        return copy;
     }
 }
