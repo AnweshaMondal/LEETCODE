0ef6b6874d089024a392c8e209f3f92b2871270a
Anwesha Mondal <159179735+AnweshaMondal@users.noreply.github.com>
2025-05-03 16:56:30 +0530

    Time: 0 ms (100%), Space: 42.4 MB (30.62%) - LeetHub

---
+++ 0086-partition-list/0086-partition-list.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+//Brute Force Idea:
+//Traverse the list and store nodes with values less than x in one list.
+// Store nodes with values greater than or equal to x in another list.
+// Join the two lists together to create the final reordered list.
+
+
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+
+        ListNode lessHead = new ListNode(-1);
+        ListNode greaterHead = new ListNode(-1);
+
+        ListNode temp = head;
+        ListNode ptr1 =  lessHead;
+        ListNode ptr2 =  greaterHead;
+
+   //Cretae 2 sorted list
+        while(temp != null)
+        {
+            if(temp.val < x)
+            {
+              ListNode newnode = new ListNode(temp.val);//create new node
+              ptr1.next = newnode;
+              ptr1 = ptr1.next;
+              temp = temp.next;
+            }
+
+            else
+            {
+              ListNode newnode = new ListNode(temp.val);//create new node
+              ptr2.next = newnode;
+              ptr2 = ptr2.next;
+              temp = temp.next;
+            }
+        }
+         
+     greaterHead = greaterHead.next;//shift its head
+
+    //merge 2 lists
+    ptr1.next = greaterHead;
+    return lessHead.next;
+
+         
+    }
+}
