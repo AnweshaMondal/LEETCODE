2107a5cf5c139ba9205ab5ef6feaba08e5560b6a
Anwesha Mondal <159179735+AnweshaMondal@users.noreply.github.com>
2025-05-03 19:38:22 +0530

    Time: 0 ms (100%), Space: 42.3 MB (42.04%) - LeetHub

--- 0086-partition-list/0086-partition-list.java
+++ 0086-partition-list/0086-partition-list.java
@@ -15,42 +15,79 @@
 // Join the two lists together to create the final reordered list.
 
 
+// class Solution {
+//     public ListNode partition(ListNode head, int x) {
+
+//         ListNode lessHead = new ListNode(-1);
+//         ListNode greaterHead = new ListNode(-1);
+
+//         ListNode temp = head;
+//         ListNode ptr1 =  lessHead;
+//         ListNode ptr2 =  greaterHead;
+
+//    //Cretae 2 sorted list
+//         while(temp != null)
+//         {
+//             if(temp.val < x)
+//             {
+//               ListNode newnode = new ListNode(temp.val);//create new node
+//               ptr1.next = newnode;
+//               ptr1 = ptr1.next;
+//               temp = temp.next;
+//             }
+
+//             else
+//             {
+//               ListNode newnode = new ListNode(temp.val);//create new node
+//               ptr2.next = newnode;
+//               ptr2 = ptr2.next;
+//               temp = temp.next;
+//             }
+//         }
+         
+//      greaterHead = greaterHead.next;//shift its head
+
+//     //merge 2 lists
+//     ptr1.next = greaterHead;
+//     return lessHead.next;
+
+         
+//     }
+// }
+
+//Without Using Extra Space
 class Solution {
     public ListNode partition(ListNode head, int x) {
 
-        ListNode lessHead = new ListNode(-1);
-        ListNode greaterHead = new ListNode(-1);
 
-        ListNode temp = head;
-        ListNode ptr1 =  lessHead;
-        ListNode ptr2 =  greaterHead;
+       ListNode lessHead = new ListNode(-1);
+       ListNode greaterHead = new ListNode(-1);
 
-   //Cretae 2 sorted list
-        while(temp != null)
+       ListNode ptr1 =  lessHead;//ptr to traverse
+       ListNode ptr2 =  greaterHead;
+       ListNode temp = head; 
+
+       while(temp != null)
         {
             if(temp.val < x)
             {
-              ListNode newnode = new ListNode(temp.val);//create new node
-              ptr1.next = newnode;
+              ptr1.next = temp;
               ptr1 = ptr1.next;
               temp = temp.next;
             }
 
             else
             {
-              ListNode newnode = new ListNode(temp.val);//create new node
-              ptr2.next = newnode;
+              ptr2.next = temp;
               ptr2 = ptr2.next;
               temp = temp.next;
             }
         }
-         
-     greaterHead = greaterHead.next;//shift its head
-
-    //merge 2 lists
+    greaterHead = greaterHead.next;
     ptr1.next = greaterHead;
-    return lessHead.next;
+    ptr2.next = null;
 
-         
+    return lessHead.next;//Skip the dummy node
+
     }
 }
